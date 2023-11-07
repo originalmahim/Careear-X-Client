7399607d5c542c752866c610ec42e63e5ea2a54c
originalmahim <tareqazizmahim@gmail.com>
2023-11-07 21:04:22 +0600

    added date comparing feature to prevent if deadline is over then block user to do apply

--- src/Pages/JobDetails/JobDetails.jsx
+++ src/Pages/JobDetails/JobDetails.jsx
@@ -1,5 +1,6 @@
 
 import { useContext } from "react";
+import { FaGithub, FaDribbble, FaTwitter, FaEnvelope } from 'react-icons/fa';
 import { useLoaderData } from "react-router-dom";
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
@@ -13,9 +14,28 @@ const JobDetails = () => {
           job.applicantEmail = user.email;
 
           const handleApply = () => {
+
+            const formattedDeadline = job?.deadline.replace(/(\d+)(?:st|nd|rd|th)/, '$1');
+
+            if (formattedDeadline) {
+            const deadlineDate = new Date(formattedDeadline);
+            const currentDate = new Date();
+
+            if (currentDate >= deadlineDate) {
+            if (currentDate.toDateString() !== deadlineDate.toDateString()) {
+            Swal.fire('Submission Failed', 'Application Deadline Is Over', 'error');
+            return;
+            }
+            }
+            } else {
+            console.error('Invalid deadline format');
+            }
+           
             if (job.postedPersonEmail === user.email) {
               return Swal.fire('Submission Failed', 'You Can Not Apply To your Won Posted Job', 'error');
             }
+            
+          
             MySwal.fire({
               title: 'Submit Application',
               html: (
@@ -71,87 +91,87 @@ const JobDetails = () => {
   return (
           <div className="max-w-6xl lg:grid justify-center gap-10 lg:grid-cols-10 mx-auto my-8" style={{ overflowX: 'hidden' }} >
           <div className=" lg:col-span-7 shadow-lg  overflow-hidden">
-  <div className=" p-4">
-    <div>
-      <div className="flex">
-        <div className="w-8/12">
+          <div className=" p-4">
+          <div>
+          <div className="flex">
+          <div className="w-8/12">
           <h5 className="mb-1 font-bold text-xl">{job?.jobTitle}</h5>
           <ul className="list-inside list-disc text-muted mb-0">
-            <li><i className="mdi mdi-account"></i> {job?.totalVacancy} Vacancy</li>
+          <li><i className="mdi mdi-account"></i> {job?.totalVacancy} Vacancy</li>
           </ul>
-        </div>
-        <div className="w-4/12 text-right">
+          </div>
+          <div className="w-4/12 text-right">
           <ul className="list-inline mb-0 mt-3 mt-lg-0">
-            <li className="list-inline-item">
-              <div className="favorite-icon">
-                <a href="javascript:void(0)"><i className="uil uil-heart-alt"></i></a>
-              </div>
-            </li>
-            <li className="list-inline-item">
-            </li>
+          <li className="list-inline-item">
+          <div className="favorite-icon">
+          <a href="javascript:void(0)"><i className="uil uil-heart-alt"></i></a>
+          </div>
+          </li>
+          <li className="list-inline-item">
+          </li>
           </ul>
-        </div>
-      </div>
-    </div>
+          </div>
+          </div>
+          </div>
 
-    <div className="mt-4">
-      <div className="flex gap-2">
-        <div className="w-3/12 border rounded-start p-3">
+          <div className="mt-4">
+          <div className="flex gap-2">
+          <div className="w-3/12 border rounded-start p-3">
           <p className="text-muted mb-0 fs-13">Experience</p>
           <p className="  ">Minimum 1 Year</p>
-        </div>
-        <div className="w-3/12 border p-3">
+          </div>
+          <div className="w-3/12 border p-3">
           <p className="text-muted fs-13 ">Employee type</p>
           <p className="text-green-500 font-semibold ">{job?.jobType}</p>
-        </div>
-        <div className="w-3/12 border p-3">
+          </div>
+          <div className="w-3/12 border p-3">
           <p className="text-muted fs-13 ">Position</p>
           <p className=" ">Senior</p>
-        </div>
-        <div className="w-3/12 border rounded-end p-3">
+          </div>
+          <div className="w-3/12 border rounded-end p-3">
           <p className="text-muted fs-13 ">Offer Salary</p>
           <p className="text-blue-500 font-semibold ">${job?.salary
-}/ Month</p>
-        </div>
-      </div>
-    </div>
+          }/ Month</p>
+          </div>
+          </div>
+          </div>
 
-    <div className="mt-4">
-      <h5 className="mb-3 font-bold text-xl">Job Description</h5>
-      <div className="job-detail-desc">
-        <p className=" mb-0 lg:text-xl">{job?.Description}</p>
-      </div>
-    </div>
-    <div className="my-3 ">
-    <img src={job?.officeImg}  className="lg:w-full lg:h-[460px]" />
-  </div>
+          <div className="mt-4">
+          <h5 className="mb-3 font-bold text-xl">Job Description</h5>
+          <div className="job-detail-desc">
+          <p className=" mb-0 lg:text-xl">{job?.Description}</p>
+          </div>
+          </div>
+          <div className="my-3 ">
+          <img src={job?.officeImg}  className="lg:w-full lg:h-[460px]" />
+          </div>
 
-    <div className="mt-4">
-      <h5 className="mb-3 font-bold text-xl">Responsibilities</h5>
-      <div className="mt-2 lg:text-xl">
-        <p className="">As a Product Designer, you will work within a Product Delivery Team fused with UX, engineering, product and data talent.</p>
-        <ul className="">
+          <div className="mt-4">
+          <h5 className="mb-3 font-bold text-xl">Responsibilities</h5>
+          <div className="mt-2 lg:text-xl">
+          <p className="">As a Product Designer, you will work within a Product Delivery Team fused with UX, engineering, product and data talent.</p>
+          <ul className="">
           <li> Have sound knowledge of commercial activities.</li>
           <li> Build next-generation web applications with a focus on the client side</li>
           <li> Work on multiple projects at once, and consistently meet draft deadlines</li>
           <li> have already graduated or are currently in any year of study</li>
           <li> Revise the work of previous designers to create a unified aesthetic for our brand materials</li>
-        </ul>
-      </div>
-    </div>
+          </ul>
+          </div>
+          </div>
 
-    <div className="mt-4">
-      <h5 className="mb-3 font-bold text-xl">Qualification</h5>
-      <div className="job-detail-desc mt-2 lg:text-xl">
-        <ul className="job-detail-list list-inside list-disc text-muted">
+          <div className="mt-4">
+          <h5 className="mb-3 font-bold text-xl">Qualification</h5>
+          <div className="job-detail-desc mt-2 lg:text-xl">
+          <ul className="job-detail-list list-inside list-disc text-muted">
           <li> {job?.EducationQualifications}</li>
           <li> 3 or more years of professional design experience</li>
           <li> have already graduated or are currently in any year of study</li>
           <li> Advanced degree or equivalent experience </li>
-        </ul>
-      </div>
-    </div>
-  </div>
+          </ul>
+          </div>
+          </div>
+          </div>
           </div>
           <div className="lg:col-span-3 lg:mt-0 mt-4 lg:items-start flex items-center justify-center lg:justify-normal">
           <div>
@@ -163,30 +183,22 @@ const JobDetails = () => {
           </div>
           <div className="flex justify-center pt-2 space-x-4 align-center">
           <a rel="noopener noreferrer" href="#" aria-label="GitHub" className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400">
-          <svg viewBox="0 0 496 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
-          <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
-          </svg>
-          </a>
-          <a rel="noopener noreferrer" href="#" aria-label="Dribble" className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400">
-          <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
-          <path d="M256 8C119.252 8 8 119.252 8 256s111.252 248 248 248 248-111.252 248-248S392.748 8 256 8zm163.97 114.366c29.503 36.046 47.369 81.957 47.835 131.955-6.984-1.477-77.018-15.682-147.502-6.818-5.752-14.041-11.181-26.393-18.617-41.614 78.321-31.977 113.818-77.482 118.284-83.523zM396.421 97.87c-3.81 5.427-35.697 48.286-111.021 76.519-34.712-63.776-73.185-116.168-79.04-124.008 67.176-16.193 137.966 1.27 190.061 47.489zm-230.48-33.25c5.585 7.659 43.438 60.116 78.537 122.509-99.087 26.313-186.36 25.934-195.834 25.809C62.38 147.205 106.678 92.573 165.941 64.62zM44.17 256.323c0-2.166.043-4.322.108-6.473 9.268.19 111.92 1.513 217.706-30.146 6.064 11.868 11.857 23.915 17.174 35.949-76.599 21.575-146.194 83.527-180.531 142.306C64.794 360.405 44.17 310.73 44.17 256.323zm81.807 167.113c22.127-45.233 82.178-103.622 167.579-132.756 29.74 77.283 42.039 142.053 45.189 160.638-68.112 29.013-150.015 21.053-212.768-27.882zm248.38 8.489c-2.171-12.886-13.446-74.897-41.152-151.033 66.38-10.626 124.7 6.768 131.947 9.055-9.442 58.941-43.273 109.844-90.795 141.978z"></path>
-          </svg>
-          </a>
-          <a rel="noopener noreferrer" href="#" aria-label="Twitter" className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400">
-          <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
-          <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
-          </svg>
-          </a>
-          <a rel="noopener noreferrer" href="#" aria-label="Email" className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400">
-          <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current">
-          <path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path>
-          </svg>
-          </a>
+        <FaGithub />
+      </a>
+      <a rel="noopener noreferrer" href="#" aria-label="Dribble" className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400">
+        <FaDribbble />
+      </a>
+      <a rel="noopener noreferrer" href="#" aria-label="Twitter" className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400">
+        <FaTwitter />
+      </a>
+      <a rel="noopener noreferrer" href="#" aria-label="Email" className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400">
+        <FaEnvelope />
+      </a>
           </div>
           </div>
           <div>
           <h1>Posted : {job?.postingDate} Days A</h1>
-          <h1>Application Deadline : {job?.deadline}</h1>
+          <h1>Deadline : {job?.deadline}</h1>
           <hr />
           </div>
           <div className="mt-4">
@@ -195,7 +207,7 @@ const JobDetails = () => {
           <ul className="list-none p-0">
           <li className="mb-2">
           <div className="flex">
-          
+
           <div>
           <p className="text-gray-500 break-words mb-0">Email : {job?.email}</p>
           <p className="text-gray-500 mt-1 break-words mb-0">Website : {job?.website}</p>
--- src/Pages/PostJob/PostJob.jsx
+++ src/Pages/PostJob/PostJob.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { AuthContex } from './../../Provider/AuthProvider';
 import axios from "axios";
 import Swal from 'sweetalert2';
-
+import moment from 'moment';
 const PostJob = () => {
           const {user} = useContext(AuthContex)
           const handlePost = e => {
@@ -20,7 +20,7 @@ const PostJob = () => {
           const jobTitle = form.jobTitle.value;
           const jobType = form.jobType.value;
           const postingDate = form.postingDate.value;
-          const deadline = form.deadline.value;
+          const deadline = moment(form.deadline.value).format("MMM Do YYYY")
           const salary = form.salary.value;
           const applicantsNumber = "0";
           const postedBy = user.displayName;
@@ -58,99 +58,99 @@ const PostJob = () => {
           }
           return (
           <div className=" bg-orange-200">
-            <div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ede9fe" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,208C384,192,480,192,576,202.7C672,213,768,235,864,224C960,213,1056,171,1152,176C1248,181,1344,235,1392,261.3L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
-            </div>
-<div className="container mx-auto mt-10 p-4 bg-orange-200 shadow-md rounded-lg ">
-      <div className="bg-white rounded-lg shadow-md p-6">
-        <h2 className="text-2xl font-semibold text-center text-gray-800 mb-4">Post A Job</h2>
-        <form onSubmit={handlePost}>
+          <div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ede9fe" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,208C384,192,480,192,576,202.7C672,213,768,235,864,224C960,213,1056,171,1152,176C1248,181,1344,235,1392,261.3L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+          </div>
+          <div className="container mx-auto mt-10 p-4 bg-orange-200 shadow-md rounded-lg ">
+          <div className="bg-white rounded-lg shadow-md p-6">
+          <h2 className="text-2xl font-semibold text-center text-gray-800 mb-4">Post A Job</h2>
+          <form onSubmit={handlePost}>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            {/* Category */}
-            <div>
-              <label htmlFor="category" className="text-gray-600 font-medium">Job Category:</label>
-              <input type="text" id="category" name="category" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            {/* Brand Image */}
-            <div>
-              <label htmlFor="brandImg" className="text-gray-600 font-medium">Company Image URL:</label>
-              <input type="text" id="brandImg" name="brandImg" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            {/* Responsibilities */}
-            <div>
-              <label htmlFor="responsibilities" className="text-gray-600 font-medium">Job Responsibilities:</label>
-              <textarea id="responsibilities" name="responsibilities" className="border border-gray-300 rounded-md p-2 w-full h-24"></textarea>
-            </div>
-            {/* Education Qualifications */}
-            <div>
-              <label htmlFor="educationQualifications" className="text-gray-600 font-medium">Education Qualifications:</label>
-              <textarea id="EducationQualifications" name="educationQualifications" className="border border-gray-300 rounded-md p-2 w-full h-24"></textarea>
-            </div>
-            {/* Email */}
-            <div>
-              <label htmlFor="email" className="text-gray-600 font-medium">Company Email:</label>
-              <input type="email" id="email" name="email" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            {/* Headquarters */}
-            <div>
-              <label htmlFor="headquarters" className="text-gray-600 font-medium">Headquarters:</label>
-              <input type="text"  name="headquarters" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            {/* Office Image */}
-            <div>
-              <label htmlFor="officeImg" className="text-gray-600 font-medium">Office Image Url:</label>
-              <input type="text" id="officeImg" name="officeImg" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            {/* Description */}
-            <div>
-              <label htmlFor="description" className="text-gray-600 font-medium">Description:</label>
-              <textarea  name="Description" className="border border-gray-300 rounded-md p-2 w-full h-24"></textarea>
-            </div>
-            {/* Employer */}
-            <div>
-              <label htmlFor="employer" className="text-gray-600 font-medium">Company Name:</label>
-              <input type="text"  name="employer" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            {/* Job Title */}
-            <div>
-              <label htmlFor="jobTitle" className="text-gray-600 font-medium">Job Title:</label>
-              <input type="text"  name="jobTitle" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            {/* Job Type */}
-            <div>
-              <label htmlFor="jobType" className="text-gray-600 font-medium">Job Type:</label>
-              <input type="text" id="jobType" name="jobType" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            {/* Posting Date */}
-            <div>
-              <label htmlFor="postingDate" className="text-gray-600 font-medium">Posting Date:</label>
-              <input type="date"  name="postingDate" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            {/* Deadline */}
-            <div>
-              <label htmlFor="deadline" className="text-gray-600 font-medium">Deadline:</label>
-              <input type="date"  name="deadline" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            {/* Salary and Total Vacancy */}
-            <div>
-              <label htmlFor="salary" className="text-gray-600 font-medium">Salary:</label>
-              <input type="text"  name="salary" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
-            <div>
-              <label htmlFor="totalVacancy" className="text-gray-600 font-medium">Total Vacancy:</label>
-              <input type="number" name="totalVacancy" className="border border-gray-300 rounded-md p-2 w-full" />
-            </div>
+          {/* Category */}
+          <div>
+          <label htmlFor="category" className="text-gray-600 font-medium">Job Category:</label>
+          <input type="text" id="category" name="category" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          {/* Brand Image */}
+          <div>
+          <label htmlFor="brandImg" className="text-gray-600 font-medium">Company Image URL:</label>
+          <input type="text" id="brandImg" name="brandImg" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          {/* Responsibilities */}
+          <div>
+          <label htmlFor="responsibilities" className="text-gray-600 font-medium">Job Responsibilities:</label>
+          <textarea id="responsibilities" name="responsibilities" className="border border-gray-300 rounded-md p-2 w-full h-24"></textarea>
+          </div>
+          {/* Education Qualifications */}
+          <div>
+          <label htmlFor="educationQualifications" className="text-gray-600 font-medium">Education Qualifications:</label>
+          <textarea id="EducationQualifications" name="educationQualifications" className="border border-gray-300 rounded-md p-2 w-full h-24"></textarea>
+          </div>
+          {/* Email */}
+          <div>
+          <label htmlFor="email" className="text-gray-600 font-medium">Company Email:</label>
+          <input type="email" id="email" name="email" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          {/* Headquarters */}
+          <div>
+          <label htmlFor="headquarters" className="text-gray-600 font-medium">Headquarters:</label>
+          <input type="text"  name="headquarters" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          {/* Office Image */}
+          <div>
+          <label htmlFor="officeImg" className="text-gray-600 font-medium">Office Image Url:</label>
+          <input type="text" id="officeImg" name="officeImg" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          {/* Description */}
+          <div>
+          <label htmlFor="description" className="text-gray-600 font-medium">Description:</label>
+          <textarea  name="Description" className="border border-gray-300 rounded-md p-2 w-full h-24"></textarea>
+          </div>
+          {/* Employer */}
+          <div>
+          <label htmlFor="employer" className="text-gray-600 font-medium">Company Name:</label>
+          <input type="text"  name="employer" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          {/* Job Title */}
+          <div>
+          <label htmlFor="jobTitle" className="text-gray-600 font-medium">Job Title:</label>
+          <input type="text"  name="jobTitle" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          {/* Job Type */}
+          <div>
+          <label htmlFor="jobType" className="text-gray-600 font-medium">Job Type:</label>
+          <input type="text" id="jobType" name="jobType" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          {/* Posting Date */}
+          <div>
+          <label htmlFor="postingDate" className="text-gray-600 font-medium">Posting Date:</label>
+          <input type="date"  name="postingDate" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          {/* Deadline */}
+          <div>
+          <label htmlFor="deadline" className="text-gray-600 font-medium">Deadline:</label>
+          <input type="date"  name="deadline" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          {/* Salary and Total Vacancy */}
+          <div>
+          <label htmlFor="salary" className="text-gray-600 font-medium">Salary:</label>
+          <input type="text"  name="salary" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
+          <div>
+          <label htmlFor="totalVacancy" className="text-gray-600 font-medium">Total Vacancy:</label>
+          <input type="number" name="totalVacancy" className="border border-gray-300 rounded-md p-2 w-full" />
+          </div>
           </div>
 
           <div className="mt-6">
-            <button type="submit" className="bg-blue-500 text-white font-medium py-2 px-4 rounded-md">Post Job</button>
+          <button type="submit" className="bg-blue-500 text-white font-medium py-2 px-4 rounded-md">Post Job</button>
           </div>
-        </form>
-      </div>
-    </div>
-    <div>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ede9fe" fill-opacity="1" d="M0,96L48,101.3C96,107,192,117,288,112C384,107,480,85,576,101.3C672,117,768,171,864,197.3C960,224,1056,224,1152,202.7C1248,181,1344,139,1392,117.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-      </div>                              
+          </form>
+          </div>
+          </div>
+          <div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ede9fe" fill-opacity="1" d="M0,96L48,101.3C96,107,192,117,288,112C384,107,480,85,576,101.3C672,117,768,171,864,197.3C960,224,1056,224,1152,202.7C1248,181,1344,139,1392,117.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+          </div>                              
           </div>
           );
 };

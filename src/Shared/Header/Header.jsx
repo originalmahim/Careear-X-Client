113b0b355a5a2d01d104b19fce1bb040aaf39cac
originalmahim <tareqazizmahim@gmail.com>
2023-11-07 21:54:25 +0600

    Visible all the rauted in mobile devices by editin header section successfully

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -51,9 +51,9 @@ const Header = () => {
           return (
           <div>
           <div className="max-w-7xl mx-auto">
-          <header className="  p-4">
+          <header className=" p-4">
           <div className="container flex justify-between h-16 mx-auto">
-          <a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
+          <a className="flex items-center p-2">
           <h1 className="flex items-center gap-1 text-2xl font-medium uppercase">
           Career <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="flex-shrink-0 w-5 h-5 rounded-full text-violet-500 text-6xl">
           <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
@@ -74,7 +74,7 @@ const Header = () => {
           </span>Apply Now</Link> 
           }
 
-{user && (
+        {user && (
         <div
           className={`dropdown dropdown-end hover:dropdown hover:dropdown-end ${isDropdownOpen ? 'open' : ''}`}
           onMouseEnter={handleDropdownToggle}
@@ -115,11 +115,52 @@ const Header = () => {
           </Link>
           }
           </div>
-          <button className="p-4 lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-100">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-          </svg>
-          </button>
+          <div className="p-4 lg:hidden">
+          { user ? <div
+          className={`dropdown dropdown-end hover:dropdown hover:dropdown-end ${isDropdownOpen ? 'open' : ''}`}
+          onMouseEnter={handleDropdownToggle}
+          onMouseLeave={handleDropdownToggle}
+        >
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src={user.photoURL} alt="User Avatar" />
+            </div>
+          </label>
+          <ul tabIndex={0} className="mt-3 z-50 p-2 shadow menu menu-sm dropdown-content hover:dropdown-content bg-base-100 rounded-box w-52">
+            <div className="px-2 border-b pb-2">
+              <img
+                className="h-24 w-24 rounded-full mx-auto"
+                src={user.photoURL}
+                alt="User Avatar"
+              />
+              <p className="pt-2 text-lg font-semibold">Hi, {user.displayName}</p>
+              <p className="text-sm text-gray-600">{user.email}</p>
+            </div>
+            <div>
+              {links}
+              <Link to= '/postJob' className=" my-1 flex items-center uppercase gap-2">
+          <span className="relative flex-shrink-0 w-2 h-2 rounded-full bg-violet-400 text-xl">
+          <span className="absolute flex-shrink-0 w-3 h-3 rounded-full -left-1 -top-1 animate-ping bg-violet-400"></span>
+          </span>Post a Job</Link>
+            </div>
+            <div className="mt-1">
+              <Link to="/dashboard" className="btn btn-sm bg-violet-300">
+                Dashboard
+              </Link>
+              <button
+                onClick={handleLogOut}
+                className="btn btn-sm bg-violet-300"
+              >
+                LogOut
+              </button>
+            </div>
+          </ul>
+        </div> :
+          <Link to= '/login' className="btn bg-violet-400 text-white">
+          Sign In
+          </Link>
+          }
+          </div>
           </div>
           </header>                             
           </div>
